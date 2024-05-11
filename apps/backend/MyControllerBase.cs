2adce9c859ccb597fdc07b9d06b24b0741684abd
LWJerri <loginmanx@gmail.com>
2024-05-11 20:59:49 +0200

    feat(backend): Add custom `MyControllerBase` class

---
+++ apps/backend/MyControllerBase.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using MVCPro.CustomResult;
+
+public class MyControllerBase : Controller
+{
+  [NonAction]
+  public virtual UpdatedAtActionResult UpdatedAtAction(string actionName, object value) => UpdatedAtAction(actionName, routeValues: null, value: value);
+
+  [NonAction]
+  public virtual UpdatedAtActionResult UpdatedAtAction(string actionName, object routeValues, object value) => UpdatedAtAction(actionName, controllerName: null, routeValues: routeValues, value: value);
+
+  [NonAction]
+  public virtual UpdatedAtActionResult UpdatedAtAction(string actionName, string controllerName, object routeValues, object value) => new UpdatedAtActionResult(actionName, controllerName, routeValues, value);
+}
